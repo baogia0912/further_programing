b658bf8ea85a30fd67577a114bec5f2f6a410862
ASUS <s3820591@rmit.edu.vn>
2022-05-15 22:01:40 +0700

    path fix

--- src/main/java/com/assignment2/taxi_management_system/controller/CustomerController.java
+++ src/main/java/com/assignment2/taxi_management_system/controller/CustomerController.java
@@ -7,56 +7,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
-    @RequestMapping(path = "/customers", method = RequestMethod.GET)
-    public List<Customer> getAllCustomers(@RequestParam(value = "page", required = false) Optional<Integer> page,
-                                          @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return customerService.getAllCustomers(page, limit);
+    @RequestMapping(path = "/admin/customers", method = RequestMethod.GET)
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.POST)
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.POST)
     public long addCustomer(@RequestBody Customer customer){
         return customerService.saveCustomer(customer);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.DELETE)
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.DELETE)
     public long deleteCustomer(@RequestBody Customer customer){
         return customerService.deleteCustomer(customer);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.PUT)
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.PUT)
     public long updateCustomers(@RequestBody Customer customer) {
         return customerService.updateCustomer(customer);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.GET, params = "id")
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.GET, params = "id")
     public Customer findByID(@RequestParam("id") long id){
         return customerService.findByID(id);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.GET, params = "name")
-    public List<Customer> findByName(@RequestParam("name") String name,
-                                     @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                     @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return customerService.findByName(name, page, limit);
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.GET, params = "name")
+    public List<Customer> findByName(@RequestParam("name") String name){
+        return customerService.findByName(name);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.GET, params = "phone_number")
-    public List<Customer> findByPhoneNumber(@RequestParam("phone_number") String phone_number,
-                                            @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                            @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return customerService.findByPhoneNumber(phone_number, page, limit);
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.GET, params = "phone_number")
+    public List<Customer> findByPhoneNumber(@RequestParam("phone_number") String phone_number){
+        return customerService.findByPhoneNumber(phone_number);
     }
 
-    @RequestMapping(path = "/customers", method = RequestMethod.GET, params = "address")
-    public List<Customer> findByAddress(@RequestParam("address") String address,
-                                        @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                        @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return customerService.findByAddress(address, page, limit);
+    @RequestMapping(path = {"/admin/customers", "/customer/customers"}, method = RequestMethod.GET, params = "address")
+    public List<Customer> findByAddress(@RequestParam("address") String address){
+        return customerService.findByAddress(address);
     }
 }
--- src/main/java/com/assignment2/taxi_management_system/controller/InvoiceController.java
+++ src/main/java/com/assignment2/taxi_management_system/controller/InvoiceController.java
@@ -2,108 +2,29 @@ package com.assignment2.taxi_management_system.controller;
 import com.assignment2.taxi_management_system.model.Invoice;
 import com.assignment2.taxi_management_system.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-
 @RestController
 public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
-    @RequestMapping(path = "/invoices", method = RequestMethod.GET)
-    public List<Invoice> getAllInvoices(@RequestParam(value = "page", required = false) Optional<Integer> page,
-                                        @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return invoiceService.getAllInvoices(page, limit);
+    @RequestMapping(path = "/admin/invoices", method = RequestMethod.GET)
+    public List<Invoice> getAllInvoices(){
+        return invoiceService.getAllInvoices();
     }
 
-    @RequestMapping(path = "/invoices", method = RequestMethod.POST)
+    @RequestMapping(path = "/admin/invoices", method = RequestMethod.POST)
     public Long addInvoice(@RequestBody Invoice invoice){
         return invoiceService.saveInvoice(invoice);
     }
 
-    @RequestMapping(path = "/invoices", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/admin/invoices", method = RequestMethod.DELETE)
     public Long deleteInvoice(@RequestBody Invoice invoice){
         return invoiceService.deleteInvoice(invoice);
     }
 
-    @RequestMapping(path = "/invoices", method = RequestMethod.PUT)
+    @RequestMapping(path = "/admin/invoices", method = RequestMethod.PUT)
     public Long updateInvoice(@RequestBody Invoice invoice){
         return invoiceService.updateInvoice(invoice);
-    }
-
-    @RequestMapping(path = "/invoices", method = RequestMethod.GET, params = {"customer_id", "start_date", "end_date"})
-    public List<Invoice> findByCustomerInDate(@RequestParam("customer") long customer_id,
-                                              @RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                              @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date,
-                                              @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                              @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return invoiceService.findByCustomerInDate(customer_id,
-                start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()),
-                page,
-                limit);
-    }
-
-    @RequestMapping(path = "/invoices", method = RequestMethod.GET, params = {"driver_id", "start_date", "end_date"})
-    public List<Invoice> findByDriverInDate(@RequestParam("driver") long driver_id,
-                                            @RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                            @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date,
-                                            @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                            @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return invoiceService.findByDriverInDate(driver_id,
-                start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()),
-                page,
-                limit);
-    }
-
-    @RequestMapping(path = "/invoices/revenue", method = RequestMethod.GET)
-    public double findRevenueAll() {
-        return invoiceService.findRevenueAll();
-    }
-
-    @RequestMapping(path = "/invoices/revenue", method = RequestMethod.GET, params = {"start_date", "end_date"})
-    public double findRevenueInDate(@RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                    @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date) {
-        return invoiceService.findRevenueInDate(start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()));
-    }
-
-    @RequestMapping(path = "/invoices/revenue", method = RequestMethod.GET, params = {"customer_id", "start_date", "end_date"})
-    public double findRevenueByCustomerInDate(@RequestParam("customer_id") long customer_id,
-                                              @RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                              @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date) {
-        return invoiceService.findRevenueByCustomerInDate(customer_id,
-                start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()));
-    }
-
-    @RequestMapping(path = "/invoices/revenue", method = RequestMethod.GET, params = {"driver_id", "start_date", "end_date"})
-    public double findRevenueByDriverInDate(@RequestParam("driver_id") long driver_id,
-                                            @RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                            @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date) {
-        return invoiceService.findRevenueByDriverInDate(driver_id,
-                start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()));
-    }
-
-    @RequestMapping(path = "/invoices", method = RequestMethod.GET, params = "id")
-    public Invoice findByID(@RequestParam("id") long id){
-        return invoiceService.findByID(id);
-    }
-
-    @RequestMapping(path = "/invoices", method = RequestMethod.GET, params = {"start_date", "end_date"})
-    public List<Invoice> findByDate(@RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date start_date,
-                                    @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end_date,
-                                    @RequestParam(value = "page", required = false) Optional<Integer> page,
-                                    @RequestParam(value = "limit", required = false) Optional<Integer> limit){
-        return invoiceService.findByDate(start_date.toInstant().atZone(ZoneId.systemDefault()),
-                end_date.toInstant().atZone(ZoneId.systemDefault()),
-                page,
-                limit);
     }
 }
