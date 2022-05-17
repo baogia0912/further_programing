bc7839436de49ebc69279261a7feaac0ba8cd814
ASUS <s3820591@rmit.edu.vn>
2022-05-18 00:34:54 +0700

    customer test

---
+++ src/test/java/com/assignment2/taxi_management_system/controller/BookingControllerTest.java
@@ -0,0 +1,39 @@
+package com.assignment2.taxi_management_system.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+    @Test
+    void getAllBookings(Optional page, Optional limit) {
+
+    }
+
+    @Test
+    void addBooking() {
+    }
+
+    @Test
+    void deleteBooking() {
+    }
+
+    @Test
+    void updateBooking() {
+    }
+
+    @Test
+    void findByID() {
+    }
+
+    @Test
+    void setInvoice() {
+    }
+
+    @Test
+    void findByDate() {
+    }
+}
--- src/test/java/com/assignment2/taxi_management_system/controller/CustomerControllerTest.java
+++ src/test/java/com/assignment2/taxi_management_system/controller/CustomerControllerTest.java
@@ -3,29 +3,23 @@ package com.assignment2.taxi_management_system.controller;
 import com.assignment2.taxi_management_system.model.Customer;
 import com.assignment2.taxi_management_system.service.CustomerService;
 import net.minidev.json.JSONObject;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CustomerController.class)
@@ -33,20 +27,10 @@ class CustomerControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private CustomerController customerController;
 
     @MockBean
     private CustomerService customerService;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
 
     @Test
     void getAllCustomers() throws Exception {
@@ -59,12 +43,27 @@ class CustomerControllerTest {
         MvcResult mvcResult = mvc.perform(get("/admin/customers")
                 .param("page", "1")
                 .param("limit", "10")
-                .with(csrf())).andExpect(status().isOk()).andReturn();
+                .with(csrf()))
+                .andExpect(status().isOk()).andReturn();
         System.out.println(mvcResult.getResponse().getContentAsString());
+        assertEquals(customerList, customerService.getAllCustomers(Optional.empty(), Optional.empty()));
     }
 
     @Test
-    void getBooking() {
+    void getBooking() throws Exception {
+        Customer bao = new Customer(1, "Bao", "abc", "0000", null);
+        JSONObject json = new JSONObject();
+        json.put("id", "1");
+
+        Mockito.when((customerService.getBookingDetails(bao))).thenReturn(bao.getBooking());
+
+        MvcResult mvcResult = mvc.perform(get("/admin/customers/booking")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(json))
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(bao.getBooking(), customerService.getBookingDetails(bao));
     }
 
     @Test
@@ -80,29 +79,119 @@ class CustomerControllerTest {
                         .content(String.valueOf(json))
                 .with(csrf())).andExpect(status().isOk()).andReturn();
         System.out.println(mvcResult.getResponse().getContentAsString());
+        assertEquals(bao, customerService.saveCustomer(bao));
     }
 
     @Test
-    void deleteCustomer() {
+    void deleteCustomer() throws Exception {
+        Customer bao = new Customer(1, "Bao", "abc", "0000", null);
+
+        JSONObject json = new JSONObject();
+        json.put("id", "1");
+
+        Mockito.when(customerService.deleteCustomer(bao)).thenReturn(bao.getId());
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete("/admin/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(json))
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+        assertEquals(bao.getId(), customerService.deleteCustomer(bao));
     }
 
     @Test
-    void updateCustomers() {
+    void updateCustomers() throws Exception {
+        Customer c1 = new Customer(1, "Bao", "abc", "0000", null);
+
+        JSONObject json = new JSONObject();
+        json.put("id", "1");
+
+
+        Mockito.when((customerService.updateCustomer(c1))).thenReturn(c1.getId());
+        MvcResult mvcResult = mvc.perform(put("/admin/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(json))
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(c1.getId(), customerService.updateCustomer(c1));
     }
 
     @Test
-    void findByID() {
+    void findByID() throws Exception {
+        Customer c1 = new Customer(1, "Bao", "abc", "0000", null);
+
+        JSONObject json = new JSONObject();
+        json.put("id", "1");
+
+
+        Mockito.when((customerService.findByID(1))).thenReturn(c1);
+        MvcResult mvcResult = mvc.perform(get("/admin/customers")
+                .param("id", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.valueOf(json))
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(c1, customerService.findByID(1));
     }
 
     @Test
-    void findByName() {
+    void findByName() throws Exception {
+        List<Customer> customerList = new ArrayList<>();
+        Customer c1 = new Customer(1, "Bao", "abc", "0000", null);
+        Customer c2 = new Customer(2, "Bao", "abcd", "0001", null);
+        customerList.add(c1);
+        customerList.add(c2);
+
+        Mockito.when((customerService.findByName("Bao", Optional.empty(), Optional.empty()))).thenReturn(customerList);
+        MvcResult mvcResult = mvc.perform(get("/admin/customers")
+                .param("name", "Bao")
+                .param("page", "1")
+                .param("limit", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(customerList, customerService.findByName("Bao", Optional.empty(), Optional.empty()));
     }
 
     @Test
-    void findByPhoneNumber() {
+    void findByPhoneNumber() throws Exception {
+        List<Customer> customerList = new ArrayList<>();
+        Customer c1 = new Customer(1, "Bao", "abc", "0000", null);
+        Customer c2 = new Customer(2, "Binh", "abcd", "0000", null);
+        customerList.add(c1);
+        customerList.add(c2);
+
+        Mockito.when((customerService.findByPhoneNumber("0000", Optional.empty(), Optional.empty()))).thenReturn(customerList);
+        MvcResult mvcResult = mvc.perform(get("/admin/customers")
+                .param("phone_number", "0000")
+                .param("page", "1")
+                .param("limit", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(customerList, customerService.findByPhoneNumber("0000", Optional.empty(), Optional.empty()));
     }
 
     @Test
-    void findByAddress() {
+    void findByAddress() throws Exception {
+        List<Customer> customerList = new ArrayList<>();
+        Customer c1 = new Customer(1, "Bao", "abcd", "0000", null);
+        Customer c2 = new Customer(2, "Binh", "abcd", "0001", null);
+        customerList.add(c1);
+        customerList.add(c2);
+
+        Mockito.when((customerService.findByAddress("abcd", Optional.empty(), Optional.empty()))).thenReturn(customerList);
+        MvcResult mvcResult = mvc.perform(get("/admin/customers")
+                .param("address", "abcd")
+                .param("page", "1")
+                .param("limit", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())).andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+        assertEquals(customerList, customerService.findByAddress("abcd", Optional.empty(), Optional.empty()));
     }
 }
